d67dc5ac7f82ace399bef36c1e68f5a5e4c85110
Alyssa Evans <alyssa.evans@pluto.tv>
2024-11-30 21:30:19 -0800

    2024 Day 1 - Part 2

--- 2024/rust/day-01/benches/benchmarks.rs
+++ 2024/rust/day-01/benches/benchmarks.rs
@@ -7,32 +7,10 @@ fn main() {
 
 #[divan::bench]
 fn part1() {
-    part1::process(divan::black_box(include_str!(
-        "../input1.txt",
-    )))
-    .unwrap();
+    part1::process(divan::black_box(include_str!("../input1.txt",))).unwrap();
 }
 
 #[divan::bench]
 fn part2() {
-    part2::process(divan::black_box(include_str!(
-        "../input2.txt",
-    )))
-    .unwrap();
-}
-
-#[divan::bench]
-fn part2_aho_corasick() {
-    part2_aho_corasick::process(divan::black_box(
-        include_str!("../input2.txt",),
-    ))
-    .unwrap();
-}
-
-#[divan::bench]
-fn part2_nom() {
-    part2_nom::process(divan::black_box(include_str!(
-        "../input2.txt",
-    )))
-    .unwrap();
+    part2::process(divan::black_box(include_str!("../input2.txt",))).unwrap();
 }
--- 2024/rust/day-01/src/part2.rs
+++ 2024/rust/day-01/src/part2.rs
@@ -1,26 +1,71 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    Ok("0".to_string())
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    // u16
+    let mut left: Vec<i32> = vec![];
+    let mut right: Vec<i32> = vec![];
+    let mut sum = 0;
+
+    let _ = input
+        .lines()
+        .inspect(|line| {
+            // dbg!(line);
+            let nums = line
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            left.push(nums[0]);
+            right.push(nums[1]);
+        })
+        // .flat_map(|line| {
+        // })
+        .collect::<Vec<_>>();
+
+    // left.sort();
+    // right.sort();
+
+    // dbg!(&left, &right);
+
+    for n in 0..left.len() {
+    dbg!(left[n]);
+        //     // dbg!(right[n]);
+        let z = dbg!(right.iter().filter(|&x| x == &left[n]).count());
+
+        //     // dbg!(i32::abs(left[n] - right[n]));
+
+        //     sum += i32::abs(left[n] - right[n]);
+
+        //     // dbg!((left[n] - right[n]).abs());
+
+        sum += dbg!(left[n] * z as i32);
+    }
+
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use rstest::rstest;
+    // use rstest::rstest;
 
-    #[rstest]
-    #[case("", "")]
-    fn test_cases(#[case] input: &str, #[case] expected: &str) {
-        assert_eq!(process(input).unwrap(), expected);
-    }
+    // #[rstest]
+    // #[case("", "")]
+    // fn test_cases(#[case] input: &str, #[case] expected: &str) {
+    //     assert_eq!(process(input).unwrap(), expected);
+    // }
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
