4b399eb3a0fd1637d152a917c777d2d81db2e14b
Alyssa Evans <alyssa.evans@pluto.tv>
2024-11-30 21:56:27 -0800

    2024 Day 1 - refactor

--- 2024/rust/day-01/src/part1.rs
+++ 2024/rust/day-01/src/part1.rs
@@ -2,45 +2,27 @@ use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    // u16
-    let mut left: Vec<i32> = vec![];
-    let mut right: Vec<i32> = vec![];
-    let mut sum = 0;
-
-    let _ = input
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = input
         .lines()
-        .inspect(|line| {
-            // dbg!(line);
-            let nums = dbg!(
-                line.split_ascii_whitespace()
-                    .map(|x| x.parse::<i32>().unwrap())
-                    .collect::<Vec<i32>>()
-            );
-
-            left.push(nums[0]);
-            right.push(nums[1]);
+        .map(|line| {
+            let nums = line
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            (nums[0], nums[1])
         })
-        // .flat_map(|line| {
-        // })
-        .collect::<Vec<_>>();
+        .unzip();
 
     left.sort();
     right.sort();
 
-    // dbg!(&left, &right);
+    let output = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum::<i32>();
 
-    for n in 0..left.len() {
-        // dbg!(left[n]);
-        // dbg!(right[n]);
-
-        // dbg!(i32::abs(left[n] - right[n]));
-
-        sum += i32::abs(left[n] - right[n]);
-
-        // dbg!((left[n] - right[n]).abs());
-    }
-
-    Ok(sum.to_string())
+    Ok(output.to_string())
 }
 
 #[cfg(test)]
@@ -50,16 +32,15 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("4", "3  7")]
-    // #[case("6", (9, 3))]
-    // #[case("2", (1, 3))]
-    // #[case("1", (2, 3))]
-    // #[case("0", (3, 3))]
-    // #[case("1", (3, 4))]
-    // #[case("2", (3, 5))]
-    // #[case("5", (4, 9))]
-    // fn test_cases(#[case] expected: &str, #[case] input: (u32, u32)) {
-    fn test_cases(#[case] expected: &str, #[case] input: &str) {
+    #[case("3  7", "4")]
+    #[case("9  3", "6")]
+    #[case("1  3", "2")]
+    #[case("2  3", "1")]
+    #[case("3  3", "0")]
+    #[case("3  4", "1")]
+    #[case("3  5", "2")]
+    #[case("4  9", "5")]
+    fn test_cases(#[case] input: &str, #[case] expected: &str) {
         assert_eq!(process(input).unwrap(), expected);
     }
 
