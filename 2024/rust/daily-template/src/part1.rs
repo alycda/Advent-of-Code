7336b1468314acfd7f1521e40441fe2ceb12a50c
Alyssa Evans <alyssa.evans@pluto.tv>
2024-11-30 22:18:47 -0800

    add rstest to template

--- 2024/rust/daily-template/src/part1.rs
+++ 2024/rust/daily-template/src/part1.rs
@@ -1,15 +1,21 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
+pub fn process(_input: &str) -> miette::Result<String, AocError> {
     todo!("day 00 - part 1");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("", "")]
+    fn test_cases(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(process(input).unwrap(), expected);
+    }
 
     #[test]
     fn test_process() -> miette::Result<()> {
