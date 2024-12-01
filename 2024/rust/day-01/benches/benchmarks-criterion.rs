243669e1d5bfe9f097cd20ec59e294a5bb2de9c6
Alyssa Evans <alyssa.evans@pluto.tv>
2024-11-30 21:31:54 -0800

    2024 Day 1 - bench

--- 2024/rust/day-01/benches/benchmarks-criterion.rs
+++ 2024/rust/day-01/benches/benchmarks-criterion.rs
@@ -1,15 +1,11 @@
-use criterion::{
-    criterion_group, criterion_main, Criterion,
-};
+use criterion::{Criterion, criterion_group, criterion_main};
 use day_01::*;
 
 fn criterion_benchmark_part1(c: &mut Criterion) {
     let input = include_str!("../input1.txt");
 
     let mut group = c.benchmark_group("day_01::part1");
-    group.bench_with_input("part1", input, |b, input| {
-        b.iter(|| part1::process(input))
-    });
+    group.bench_with_input("part1", input, |b, input| b.iter(|| part1::process(input)));
 
     group.finish();
 }
@@ -18,21 +14,7 @@ fn criterion_benchmark_part2(c: &mut Criterion) {
     let input = include_str!("../input2.txt");
 
     let mut group = c.benchmark_group("day_01::part2");
-    group.bench_with_input("part2", input, |b, input| {
-        b.iter(|| part2::process(input))
-    });
-    group.bench_with_input(
-        "part2_nom",
-        input,
-        |b, input| b.iter(|| part2_nom::process(input)),
-    );
-    group.bench_with_input(
-        "part2_aho_corasick",
-        input,
-        |b, input| {
-            b.iter(|| part2_aho_corasick::process(input))
-        },
-    );
+    group.bench_with_input("part2", input, |b, input| b.iter(|| part2::process(input)));
     group.finish();
 }
 
