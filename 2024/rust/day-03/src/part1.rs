f316206a4a641a812f15f217fc4909474d83d3ac
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-02 23:04:06 -0800

    2024 Day 3 - part 1 nom

--- 2024/rust/day-03/src/part1.rs
+++ 2024/rust/day-03/src/part1.rs
@@ -2,21 +2,11 @@ use nom::{
     IResult,
     bytes::complete::{tag, take_until},
     character::complete::{char, digit1},
+    error::{Error, ErrorKind},
     sequence::{preceded, terminated, tuple},
 };
 
 use crate::custom_error::AocError;
-
-// fn _mul(a: usize, b: usize) -> usize {
-//     a * b
-// }
-
-// fn recursive_find(input: &str) -> Option<&str> {
-//     match input.find("mul(") {
-//         Some(idx) => recursive_find(&input[idx + 3..]),
-//         _ => None,
-//     }
-// }
 
 fn parse_mul(input: &str) -> IResult<&str, (&str, &str)> {
     let (input, _) = take_until("mul(")(input)?;
@@ -34,72 +24,136 @@ fn parse_mul(input: &str) -> IResult<&str, (&str, &str)> {
 }
 
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    if let Ok((remainder, (a, b))) = dbg!(parse_mul(input)) {
-        let a = dbg!(a.parse::<usize>().unwrap());
-        let b = dbg!(b.parse::<usize>().unwrap());
+pub fn process(mut input2: &str) -> miette::Result<String, AocError> {
+    let mut sum = Vec::new();
 
-        return Ok((a * b).to_string());
+    // if let Ok((remainder, (a, b))) = dbg!(parse_mul(input)) {
+    //     let a = dbg!(a.parse::<usize>().unwrap());
+    //     let b = dbg!(b.parse::<usize>().unwrap());
+
+    //     return Ok((a * b).to_string());
+    // }
+
+    // while input.len() > 3 {
+    while !input2.is_empty() {
+        match parse_mul(input2) {
+            Ok((remainder, (a, b))) => {
+                let a = a.parse::<usize>().unwrap();
+                let b = b.parse::<usize>().unwrap();
+
+                sum.push(a * b);
+
+                // input2 = dbg!(remainder);
+                input2 = remainder;
+            }
+            Err(e) => {
+                // panic!("{e:?}");
+
+                if let nom::Err::Error(err) = &e {
+                    match err {
+                        Error {
+                            input: _,
+                            code: ErrorKind::Char,
+                        } => {
+                            if input2.len() > 1 {
+                                input2 = &input2[1..];
+                            } else {
+                                panic!("{e:?}");
+                            }
+                        }
+                        Error {
+                            input: _,
+                            code: ErrorKind::TakeUntil,
+                        } => {
+                            // input = &input[1..];
+                            input2 = "";
+                            // break;
+                        }
+                        _ => panic!("{e:?}"),
+                    }
+                } else {
+                    panic!("{e:?}");
+                }
+
+                // invalid terminator but string is not empty
+
+                // e.map(|err| {
+                //     // if let ErrorKind::Char = err.code
+                //     //     && input.len() > 1
+                //     // {
+                //     //     input = &input[1..];
+                //     // } else {
+                //     //     panic!("{e:?}");
+                //     // }
+
+                //     match err.code {
+                //         ErrorKind::Char => {
+                //             if input.len() > 1 {
+                //                 input = &input[1..];
+                //             } else {
+                //                 panic!("{e:?}");
+                //             }
+                //         }
+                //         _ => panic!("{e:?}"),
+                //     }
+                // });
+
+                // if e.code == ErrorKind::Char && input.len() > 1 {
+                //     input = &input[1..];
+                // } else {
+                //     panic!("{e:?}");
+                // }
+
+                // if let nom::Err::Error(err) = ErrorKind::Char {
+                //     input = &input[1..];
+                // } else {
+                //     panic!("{e:?}");
+                // }
+
+                // (input, _) = take_until::<&str, &str, Error<&str>>("mul(")(input).unwrap();
+
+                // match e {
+                //     nom::Err::Error(err) => {
+                //         dbg!(err);
+                //         // input = &input[1..];
+                //         input = &input[1..];
+                //     }
+                //     _ => todo!("moar"), // nom::Err::Failure(_) => {
+                //                         //     // input = &input[1..];
+                //                         //     input = &input[1..];
+                //                         // }
+                //                         // nom::Err::Incomplete(_) => {
+                //                         //     // input = &input[1..];
+                //                         //     input = &input[1..];
+                //                         // }
+                // }
+
+                // if input.len() > 1 {
+                //     input = &input[1..];
+                // } else {
+                //     break;
+                // }
+                // continue;
+            }
+        }
     }
 
-    // let indices: Vec<_> = input
-    //     .match_indices("mul(")
-    //     .inspect(|(idx, s)| {
-    //         dbg!(idx, s);
+    // while let Ok((remainder, (a, b))) = parse_mul(input) {
+    //     let a = a.parse::<usize>().unwrap();
+    //     let b = b.parse::<usize>().unwrap();
 
-    //         // dbg!(&input[idx + 3..]);
-    //         dbg!()
-    //     })
-    //     // .filter_map(|(idx, s)| {
-    //     // })
-    //     .collect();
+    //     sum.push(a * b);
 
-    Ok("".to_string())
+    //     input = dbg!(remainder);
+    // }
 
-    // let mut peekable = input.chars().peekable();
+    // if let Err(e) = parse_mul(input) {
+    //     dbg!(e);
+    // }
 
-    // let mut vals: Vec<(usize, usize)> = vec![];
-    // let mut tmp: String::new();
+    // dbg!(&sum);
 
-    // let output: usize = input
-    //     .chars()
-    //     .flat_map(|c| {
-
-    //     })
-    //     // .fold(vals, |c| {
-    //     //     if c == 'm' {
-
-    //     //     }
-    //     // })
-    //     .map(mul).sum();
-    //     // .enumerate()
-    //     // .flat_map(|(idx, c)| {
-    //     // })
-    //     // .inspect(|(idx, c)| {
-    //     //     if *c == 'm' {
-    //     //         dbg!(idx);
-    //     //         let _ = peekable.advance_by(*idx + 1);
-
-    //     //         if peekable.peek().unwrap() == &'u' {
-    //     //             dbg!(peekable.peek());
-    //     //             peekable.next();
-
-    //     //             if peekable.peek().unwrap() == &'l' {
-    //     //                 dbg!(peekable.peek());
-    //     //                 peekable.next();
-    //     //             }
-    //     //         }
-
-    //     //         // dbg!(peekable.peek());
-    //     //         // peekable.next();
-    //     //         // dbg!(peekable.peek());
-    //     //         // peekable.next();
-    //     //         // dbg!(peekable.peek());
-    //     //     }
-    //     // })
-    //     // .count();
-
-    // Ok(output.to_string())
+    Ok(sum.iter().sum::<usize>().to_string())
 }
 
 #[cfg(test)]
@@ -111,11 +165,14 @@ mod tests {
     #[rstest]
     #[case("mul(2,4)", "8")]
     #[case("mul(5,5)", "25")]
-    #[case("mul(32,64]", "")]
+    #[case("mul(32,64].....", "0")]
     #[case("mul(11,8)", "88")]
     #[case("mul(8,5)", "40")]
     #[case("xmul(2,4)%", "8")]
     #[case("xmul(2,4)%&mul[3,7]", "8")]
+    #[case("+mul(32,64]then(", "0")]
+    #[case("+mul(32,64]then(mul(11,8)mul(8,5))", "128")]
+    #[case("]then(mul(11,8)mul(8,5))", "128")]
     fn test_cases(#[case] input: &str, #[case] expected: &str) {
         assert_eq!(process(input).unwrap(), expected);
     }
