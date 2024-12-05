81928749042a0e2567600ef0079603bb244bb99c
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-04 23:09:25 -0800

    bench-all

--- 2024/rust/day-04/benches/benchmarks-criterion.rs
+++ 2024/rust/day-04/benches/benchmarks-criterion.rs
@@ -14,31 +14,31 @@ fn criterion_benchmark_part1(c: &mut Criterion) {
     group.finish();
 }
 
-fn criterion_benchmark_part2(c: &mut Criterion) {
-    let input = include_str!("../input2.txt");
+// fn criterion_benchmark_part2(c: &mut Criterion) {
+//     let input = include_str!("../input2.txt");
 
-    let mut group = c.benchmark_group("day_04::part2");
-    group.bench_with_input("part2", input, |b, input| {
-        b.iter(|| part2::process(input))
-    });
-    // group.bench_with_input(
-    //     "part2_nom",
-    //     input,
-    //     |b, input| b.iter(|| part2_nom::process(input)),
-    // );
-    // group.bench_with_input(
-    //     "part2_aho_corasick",
-    //     input,
-    //     |b, input| {
-    //         b.iter(|| part2_aho_corasick::process(input))
-    //     },
-    // );
-    group.finish();
-}
+//     let mut group = c.benchmark_group("day_04::part2");
+//     group.bench_with_input("part2", input, |b, input| {
+//         b.iter(|| part2::process(input))
+//     });
+//     // group.bench_with_input(
+//     //     "part2_nom",
+//     //     input,
+//     //     |b, input| b.iter(|| part2_nom::process(input)),
+//     // );
+//     // group.bench_with_input(
+//     //     "part2_aho_corasick",
+//     //     input,
+//     //     |b, input| {
+//     //         b.iter(|| part2_aho_corasick::process(input))
+//     //     },
+//     // );
+//     group.finish();
+// }
 
 criterion_group!(
     benches,
     criterion_benchmark_part1,
-    criterion_benchmark_part2
+    // criterion_benchmark_part2
 );
 criterion_main!(benches);
