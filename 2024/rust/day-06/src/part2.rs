f3372e4ede4dc5dcf9a88e001d300cd23edac4c7
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-06 23:44:11 -0800

    2024 Day 6 - Part 2

--- 2024/rust/day-06/src/part2.rs
+++ 2024/rust/day-06/src/part2.rs
@@ -1,17 +1,15 @@
 use std::{collections::{HashMap, HashSet}, hash::Hash};
 
-// use rayon::prelude::*;
-
 use glam::IVec2;
 
 use crate::custom_error::AocError;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
-    Up, // A
-    Down, // X
-    Left, // #
-    Right, // O
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 impl Direction {
@@ -24,7 +22,7 @@ impl Direction {
         }
     }
 
-    fn right_turn(&self) -> Self {
+    fn turn_right(&self) -> Self {
         match self {
             Direction::Up => Direction::Right,
             Direction::Right => Direction::Down,
@@ -44,15 +42,12 @@ impl Grid {
     }
 
     fn to_position(&self, idx: usize) -> IVec2 {
-
         let cols = self.get_cols();
-        // let chars_per_row = cols + 1;
         let col = idx % cols;
         let row = idx / cols;
         IVec2::new(col as i32, row as i32)
     }
 
-    // chars per row is cols + 1
     fn get_cols(&self) -> usize {
         self.0 + 1
     }
@@ -61,165 +56,105 @@ impl Grid {
         self.1
     }
 
+    fn get_bounds(&self) -> ((i32, i32), (i32, i32)) {
+        let max_x = self.get_cols() as i32 - 1;
+        let max_y = self.get_rows() as i32 - 1;
+        ((0, max_x), (0, max_y))
+    }
+
     // fn place_obstacles(&self, start_position: IVec2, existing_walls: &HashSet<IVec2>) -> usize {
     //     self.2.iter()
-    //         .take(1)
     //         .filter(|(new_obstacle, _)| {
-
     //             let mut new_walls = existing_walls.clone();
-    //             new_walls.insert(new_obstacle.clone());
-    //             let ref mut new_grid = Grid::new(self.get_cols(), self.get_rows());
-
-    //             // start position
-    //             sentry(start_position, Direction::Up, new_grid, &new_walls);
-
-    //             dbg!(&self.2, &new_grid.2);
-
-    //             false
-    //         }).count()
+    //             new_walls.insert(*new_obstacle);
+    //             sentry(start_position, Direction::Up, &new_walls, new_obstacle, self.get_bounds())
+    //         })
+    //         .count()
     // }
 
     fn place_obstacles(&self, start_position: IVec2, existing_walls: &HashSet<IVec2>) -> usize {
+        // dbg!(self.get_bounds());
         self.2.iter()
             .filter(|(new_obstacle, _)| {
+                dbg!(new_obstacle);
                 let mut new_walls = existing_walls.clone();
-                new_walls.insert(new_obstacle.clone());
-                let mut new_grid = Grid::new(self.get_cols(), self.get_rows());
-
-                // Start position
-                if let Some(cycle_detected) = sentry_2(start_position, Direction::Up, &mut new_grid, &new_walls, &self.2) {
-                    cycle_detected
-                } else {
-                    false
-                }
+                new_walls.insert(*new_obstacle);
+                let result = sentry(start_position, Direction::Up, &new_walls, &new_obstacle, self.get_bounds());
+                dbg!(result);
+                result
             })
             .count()
     }
 
     fn insert(&mut self, pos: IVec2, cardinal: Direction) {
-        // self.2.insert(pos);
         self.2.push((pos, cardinal));
     }
 
     fn exit(&self, pos: IVec2, dir: Direction) -> bool {
-        let cols = self.get_cols();
-        let rows = self.get_rows();
         let col = pos.x as usize;
         let row = pos.y as usize;
 
         match dir {
-            Direction::Up => {
-                if row == 0 {
-                    return true;
-                }
-            },
-            Direction::Down => {
-                if row + 1 == rows {
-                    return true;
-                }
-            },
-            Direction::Left => {
-                if col == 0 {
-                    return true;
-                }
-            },
-            Direction::Right => {
-                if col + 1 == cols {
-                    return true;
-                }
-            }
-        }
-
-        false
-    }
-    
-}
-
-fn sentry(mut position: IVec2, mut direction: Direction, grid: &mut Grid, walls: &HashSet<IVec2>) {
-    loop {
-        if grid.exit(position, direction) {
-            break;
-        }
-
-        let next_position = position + direction.to_position();
-
-        if let Some(_) = walls.get(&next_position) {
-            // direction = Ivec2::rotate
-            direction = direction.right_turn();            
-        } else {
-            position = next_position;
-            grid.insert(position, direction);
+            Direction::Up => row == 0,
+            Direction::Down => row + 1 == self.get_rows(),
+            Direction::Left => col == 0,
+            Direction::Right => col + 1 == self.get_cols(),
         }
     }
 }
 
-fn sentry_2(mut position: IVec2, mut direction: Direction, grid: &mut Grid, walls: &HashSet<IVec2>, original_path: &[(IVec2, Direction)]) -> Option<bool> {
-    let mut visited = HashSet::new();
+fn sentry(
+    mut position: IVec2,
+    mut direction: Direction,
+    walls: &HashSet<IVec2>,
+    new_wall: &IVec2,
+    bounds: ((i32, i32), (i32, i32)),
+) -> bool {
+    // dbg!(position, direction);
+
+    let mut visited: HashSet<(IVec2, Direction)> = HashSet::new();
+    let (x_minmax, y_minmax) = bounds;
 
     loop {
-        if grid.exit(position, direction) {
-            return None;
-        }
-
-        if !visited.insert((position, direction)) {
-            return Some(true); // Found a cycle
-        }
-
-        // Check if the current position and direction match any position and direction in the original path
-        if original_path.contains(&(position, direction)) {
-            return Some(true); // Found a cycle
-        }
-
         let next_position = position + direction.to_position();
+        // dbg!(next_position, direction);
 
-        if let Some(_) = walls.get(&next_position) {
-            direction = direction.right_turn();            
-        } else {
+        if walls.contains(&next_position) || next_position == *new_wall {
+            direction = direction.turn_right();
+        } else if !visited.insert((position, direction)) {
+            return true; // Found a cycle
+        } else if (x_minmax.0..=x_minmax.1).contains(&next_position.x)
+            && (y_minmax.0..=y_minmax.1).contains(&next_position.y)
+        {
             position = next_position;
-            grid.insert(position, direction);
+        } else {
+            return false; // No cycle found
         }
+
+        dbg!(&visited);
     }
 }
 
 pub fn process(input: &str) -> miette::Result<String, AocError> {
     let mut peekable = input.lines().peekable();
     let cols = peekable.peek().unwrap().chars().count();
-    let rows = peekable
-        .count();
+    let rows = peekable.count();
 
     let mut grid = Grid::new(cols, rows);
     let start = input.find("^").unwrap();
     let walls = input.match_indices("#")
-        // .inspect(|(idx, _)| { dbg!(idx); } )
-        .map(|(idx, _)|{ grid.to_position(idx) })
-        // .inspect(|pos| { dbg!(pos); })
+        .map(|(idx, _)| grid.to_position(idx))
         .collect::<HashSet<_>>();
 
-    let mut last_position = grid.to_position(start);
+    let start_position = grid.to_position(start);
+    let direction = Direction::Up;
 
-//     let mut neighbors: Option<HashMap<&str, (Position, char)>> = Some(grid.get_neighbors(last_position));
-    let mut direction = Direction::Up;
+    // dbg!(&walls.len());
+    // dbg!(start_position);
 
-    // loop {
-    //     if grid.exit(last_position, direction) {
-    //         break;
-    //     }
+    sentry(start_position, direction, &walls, &start_position, grid.get_bounds());
 
-    //     let next_position = last_position + direction.to_position();
-
-    //     if let Some(_) = walls.get(&next_position) {
-    //         // direction = Ivec2::rotate
-    //         direction = direction.right_turn();            
-    //     } else {
-    //         last_position = next_position;
-    //         grid.insert(last_position, direction);
-    //     }
-    // }
-    sentry(last_position, direction, &mut grid, &walls);
-
-    dbg!(grid.2.len());
-
-    let output = grid.place_obstacles(grid.to_position(start), &walls);
+    let output = grid.place_obstacles(start_position, &walls);
 
     Ok(output.to_string())
 }
