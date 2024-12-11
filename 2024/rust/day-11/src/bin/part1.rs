c01eb1530c194d6d3e37312b2a90bf606617f045
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-10 21:28:04 -0800

    2024 Day 11 - Part 1

--- 2024/rust/day-11/src/bin/part1.rs
+++ 2024/rust/day-11/src/bin/part1.rs
@@ -14,7 +14,7 @@ fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
     let file = include_str!("../../input1.txt");
-    let result = process(file).context("process part 1")?;
+    let result = process(file, 25).context("process part 1")?;
     println!("{}", result);
     Ok(())
 }
--- 2024/rust/day-11/src/part1.rs
+++ 2024/rust/day-11/src/part1.rs
@@ -1,56 +1,93 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let numbers = input.split_whitespace()
-        // .map(|n| n.parse::<usize>().unwrap())
-        .inspect(|stone| {
-            dbg!(stone, stone.chars().count());
+pub fn process(input: &str, times: usize) -> miette::Result<String, AocError> {
+    // let numbers: Vec<usize> = input
+    //     .split_whitespace()
+    //     .flat_map(|stone| {
+    //         if stone == "0" {
+    //             vec![1]
+    //         } else if stone.len() % 2 == 0 {
+    //             // For even length strings, split and parse both halves
+    //             let (left, right) = stone.split_at(stone.len() / 2);
+    //             vec![
+    //                 left.parse().unwrap_or(0),
+    //                 right.parse().unwrap_or(0)
+    //             ]
+    //         } else {
+    //             // For odd length, multiply by 2024
+    //             vec![stone.parse::<usize>().unwrap_or(0) * 2024]
+    //         }
+    //     })
+    //     .collect();
 
-            if stone == &"0" {
-                [dbg!(1)];
-            } else if stone.chars().count() / 2 == 0 {
-                // split in half
-                let (left, right) = stone.split_at(stone.chars().count() / 2);
-                let left = left.parse::<usize>().unwrap();
-                let right = right.parse::<usize>().unwrap();
+    // // implement the recursion
+    // let result = blink(&numbers.iter()
+    //     .map(|n| n.to_string())
+    //     .collect::<Vec<_>>()
+    //     .join(" "), 1);
 
-                [left, right];
-            } else {
-                [dbg!(stone.parse::<usize>().unwrap() * 2024)];
-            }
-        }).count();
-        // .collect::<Vec<usize>>();
+    // Ok(result)
 
-    Ok("".to_string())
+    Ok(blink(input, times).split_whitespace().count().to_string())
 }
 
-fn _blink(_input: &str, _times: usize) -> String {
-    "".to_string()
+fn blink(input: &str, times: usize) -> String {
+    // Base case - if times is 0, return the input
+    if times == 0 {
+        return input.to_string();
+    }
+
+    // Process current iteration similar to flat_map
+    let numbers: Vec<usize> = input
+        .split_whitespace()
+        .flat_map(|stone| {
+            if stone == "0" {
+                vec![1]
+            } else if stone.len() % 2 == 0 {
+                let (left, right) = stone.split_at(stone.len() / 2);
+                vec![
+                    left.parse().unwrap(),
+                    right.parse().unwrap()
+                ]
+            } else {
+                vec![stone.parse::<usize>().unwrap() * 2024]
+            }
+        })
+        .collect();
+
+    // Convert back to string and recurse
+    let new_input = numbers.iter()
+        .map(|n| n.to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    // Recursive call with times-1
+    blink(&new_input, times - 1)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use rstest::rstest;
+    // use rstest::rstest;
 
-    #[rstest]
-    #[case(("125 17", 1), "253000 1 7")]
+    // #[rstest]
+    // #[case(("125 17", 1), "253000 1 7")]
     // #[case(("125 17", 2), "253 0 2024 14168")]
     // #[case(("125 17", 3), "512072 1 20 24 28676032")]
     // #[case(("125 17", 4), "512 72 2024 2 0 2 4 2867 6032")]
     // #[case(("125 17", 5), "1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32")]
     // #[case(("125 17", 6), "2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2")]
-    fn test_cases(#[case] input: (&str, usize), #[case] expected: &str) {
-        // assert_eq!(blink(input.0, input.1), expected);
-        assert_eq!(expected, process(input.0).unwrap());
-    }
-
-    // #[test]
-    // fn test_process() -> miette::Result<()> {
-    //     let input = "125 17";
-    //     assert_eq!("55312", process(input)?);
-    //     Ok(())
+    // fn test_cases(#[case] input: (&str, usize), #[case] expected: &str) {
+    //     // assert_eq!(blink(input.0, input.1), expected);
+    //     assert_eq!(expected, process(input.0, input.1).unwrap());
     // }
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "125 17";
+        assert_eq!("55312", process(input, 25)?);
+        Ok(())
+    }
 }
