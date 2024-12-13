639cdd48d08e5acf3c2e19a84201fcc73f658ee7
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-13 07:42:17 +0000

    refactor day 4 using ornaments (Part 2) WIP

--- 2024/rust/day-04/src/bin/part2.rs
+++ 2024/rust/day-04/src/bin/part2.rs
@@ -7,7 +7,7 @@ use day_04::{Day4, CrossPattern};
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
-#[tracing::instrument]
+// #[tracing::instrument]
 fn main() -> miette::Result<()> {
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
@@ -15,9 +15,9 @@ fn main() -> miette::Result<()> {
     #[cfg(not(feature = "dhat-heap"))]
     tracing_subscriber::fmt::init();
 
-    let file = include_str!("../../input2.txt");
-    let result = Day4::<CrossPattern>::parse(file).solve(ornaments::Part::One).context("process part 1")?;
+    // let file = include_str!("../../input2.txt");
+    // let result = Day4::<CrossPattern>::parse(file).solve(ornaments::Part::One).context("process part 1")?;
 
-    println!("{}", result);
+    // println!("{}", result);
     Ok(())
 }
--- 2024/rust/day-04/src/lib.rs
+++ 2024/rust/day-04/src/lib.rs
@@ -1,66 +1,22 @@
-use std::marker::PhantomData;
-use std::collections::HashSet;
+use std::{collections::HashSet, marker::PhantomData};
+
 use glam::IVec2;
 use ornaments::{AocError, Grid, Solution, ALL_DIRECTIONS};
 
-pub trait Pattern {
-    fn matches(&self, grid: &Grid, pos: IVec2) -> bool;
-    fn find_starting_positions(grid: &Grid) -> HashSet<IVec2>;
+pub trait Pattern: Default {
+    fn matches(&self, chars: &[char]) -> bool;
 }
 
-#[derive(Default)]
 pub struct XmasPattern;
-
-#[derive(Default)]
 pub struct CrossPattern;
 
-impl Pattern for XmasPattern {
-    fn matches(&self, grid: &Grid, pos: IVec2) -> bool {
-        ALL_DIRECTIONS.iter().any(|dir| {
-            if let Some(chars) = grid.go_straight(pos, *dir, 3) {
-                chars.iter().collect::<String>() == "MAS"
-            } else {
-                false
-            }
-        })
-    }
-
-    fn find_starting_positions(grid: &Grid) -> HashSet<IVec2> {
-        let mut positions = HashSet::new();
-        grid.walk(|pos| {
-            if grid.get_at(pos).unwrap() == 'X' {
-                positions.insert(pos);
-            }
-        });
-        positions
-    }
+impl Default for XmasPattern {
+    fn default() -> Self { Self }
 }
 
-impl Pattern for CrossPattern {
-    fn matches(&self, grid: &Grid, pos: IVec2) -> bool {
-        let diagonals = [
-            (IVec2::new(1, 1), IVec2::new(-1, -1)),
-            (IVec2::new(-1, 1), IVec2::new(1, -1)),
-        ];
-        
-        diagonals.iter().any(|(dir1, dir2)| {
-            let path1 = grid.go_straight(pos, *dir1, 3);
-            let path2 = grid.go_straight(pos, *dir2, 3);
-            
-            matches!((path1, path2), (Some(p1), Some(p2)) 
-                if p1.iter().collect::<String>() == "MAS" 
-                && p2.iter().collect::<String>() == "MAS")
-        })
-    }
-
-    fn find_starting_positions(grid: &Grid) -> HashSet<IVec2> {
-        let mut positions = HashSet::new();
-        grid.walk(|pos| {
-            if grid.get_at(pos).unwrap() == 'M' {
-                positions.insert(pos);
-            }
-        });
-        positions
+impl Pattern for XmasPattern {
+    fn matches(&self, chars: &[char]) -> bool {
+        chars.iter().collect::<String>() == "MAS"
     }
 }
 
@@ -70,18 +26,25 @@ pub struct Day4<P> {
     _pattern: PhantomData<P>
 }
 
-impl<P: Pattern + Default> Solution for Day4<P> {
+impl<P: Pattern> Solution for Day4<P> {
     type Output = usize;
     type Item = HashSet<IVec2>;
 
     fn parse(input: &'static str) -> Self {
         let grid = Self::to_grid(input);
-        let set = P::find_starting_positions(&grid);
-        assert!(!set.is_empty());
-        
+        let mut x_positions = HashSet::new();
+
+        grid.walk(|pos| {
+            if grid.get_at(pos).unwrap() == 'X' {
+                x_positions.insert(pos);
+            }
+        });
+
+        assert!(!x_positions.is_empty());
+
         Self {
             grid,
-            set,
+            set: x_positions,
             _pattern: PhantomData
         }
     }
@@ -89,8 +52,14 @@ impl<P: Pattern + Default> Solution for Day4<P> {
     fn part1(&mut self) -> Result<Self::Output, AocError> {
         let pattern = P::default();
         let output = self.set.iter()
-            .filter(|&pos| pattern.matches(&self.grid, *pos))
+            .flat_map(|pos| {
+                ALL_DIRECTIONS.iter().filter_map(|dir| {
+                    self.grid.go_straight(*pos, *dir, 3)
+                })
+            })
+            .filter(|chars| pattern.matches(chars))
             .count();
+
         Ok(output)
     }
 }
@@ -117,19 +86,19 @@ MXMXAXMASX";
         Ok(())
     }
 
-    #[test]
-    fn test_part2() -> miette::Result<()> {
-        let input = "MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX";
-        assert_eq!("9", Day4::<CrossPattern>::parse(input).solve(Part::Two)?);
-        Ok(())
-    }
+//     #[test]
+//     fn test_part2() -> miette::Result<()> {
+//         let input = "MMMSXXMASM
+// MSAMXMSMSA
+// AMXSXMAAMM
+// MSAMASMSMX
+// XMASAMXAMM
+// XXAMMXXAMA
+// SMSMSASXSS
+// SAXAMASAAA
+// MAMMMXMMMM
+// MXMXAXMASX";
+//         assert_eq!("9", Day4::<CrossPattern>::parse(input).solve(Part::Two)?);
+//         Ok(())
+//     }
 }
