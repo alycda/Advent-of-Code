015ddb3916df7f8ad8615a646209600a4e00208e
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-18 21:34:55 -0800

    2024 Day 19 - Part 2

--- 2024/rust/day-19/src/part2.rs
+++ 2024/rust/day-19/src/part2.rs
@@ -1,8 +1,41 @@
+use itertools::Itertools;
+
 use crate::AocError;
 
-#[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    Ok("".to_string())
+use std::collections::HashSet;
+
+fn count_ways(patterns: &HashSet<String>, target: &str) -> usize {
+    let n = target.len();
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+
+    for i in 1..=n {
+        for pattern in patterns {
+            let pattern_len = pattern.len();
+            if i >= pattern_len && &target[i - pattern_len..i] == pattern {
+                dp[i] += dp[i - pattern_len];
+            }
+        }
+    }
+
+    dp[n]
+}
+
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (patterns_str, lines) = input.split("\n\n").collect_tuple().unwrap();
+
+    let patterns: HashSet<String> = patterns_str
+        .split(',') 
+        .map(|s| s.trim().to_string())
+        .collect();
+
+    let output = lines
+        .lines()
+        .map(|s| s.trim().to_string())
+        .map(|line| count_ways(&patterns, &line))
+        .sum::<usize>();
+
+    Ok(output.to_string())
 }
 
 #[cfg(test)]
@@ -19,8 +52,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("0", process(input)?);
+        let input = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+        assert_eq!("16", process(input)?);
         Ok(())
     }
 }
