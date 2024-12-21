47aaa25b8423f6537bbad4478ffdca48460c8398
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-20 17:54:49 -0800

    viz day 1 WIP

---
+++ 2024/rust/lights/src/bin/day1.rs
@@ -0,0 +1,96 @@
+use bevy::prelude::*;
+
+use std::marker::PhantomData;
+
+// Marker components for our two lists
+#[derive(Component)]
+struct LeftList;
+
+#[derive(Component)]
+struct RightList;
+
+// Generic number component that can be specialized
+#[derive(Component)]
+struct Number<T: Component> {
+    value: i32,
+    index: usize,
+    _phantom: PhantomData<T>,
+}
+
+impl<T: Component> Number<T> {
+    fn new(value: i32, index: usize) -> Self {
+        Self {
+            value,
+            index,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+#[derive(Bundle)]
+struct NumberBundle<T: Component> {
+    number: Number<T>,
+    // Spatial components
+    // sprite: Text2dBundle,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        // .add_systems(Update, hello_world)
+        .run();
+}
+
+// fn hello_world() {
+//     println!("hello Pluto!");
+// }
+
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2d::default());
+
+    // Example data
+    let left = vec![1, 3, 5, 7];
+    let right = vec![2, 4, 6, 8];
+
+    // Spawn left numbers
+    for (idx, &value) in left.iter().enumerate() {
+        spawn_number::<LeftList>(&mut commands, value, idx);
+    }
+
+    // Spawn right numbers
+    for (idx, &value) in right.iter().enumerate() {
+        spawn_number::<RightList>(&mut commands, value, idx);
+    }
+}
+
+fn spawn_number<T>(commands: &mut Commands, value: i32, index: usize) 
+where T: Component
+{
+    let x_offset = if std::any::TypeId::of::<T>() == std::any::TypeId::of::<LeftList>() {
+        -300.0
+    } else {
+        300.0
+    };
+    let y_position = (index as f32) * -50.0 + 200.0;
+
+    // commands.spawn((
+    //     NumberBundle {
+    //         number: Number::<T>::new(value, index),
+    //         sprite: Text2dBundle {
+    //             text: Text::from_section(
+    //                 value.to_string(),
+    //                 TextStyle {
+    //                     font_size: 40.0,
+    //                     color: Color::WHITE,
+    //                     ..default()
+    //                 },
+    //             ),
+    //             transform: Transform::from_xyz(x_offset, y_position, 0.0),
+    //             ..default()
+    //         },
+    //     },
+    //     T::default(),
+    // ));
+}
