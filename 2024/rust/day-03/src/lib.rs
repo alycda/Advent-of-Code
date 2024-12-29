a42bdb4b7a6d96a3797f73c7c74d1c4f70f17cd3
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-29 08:43:20 +0000

    2024 day 3 refactor part 2 WIP

--- 2024/rust/day-03/src/lib.rs
+++ 2024/rust/day-03/src/lib.rs
@@ -16,7 +16,10 @@
 use std::{marker::PhantomData, num::ParseIntError, str::FromStr};
 
 use nom::{bytes::complete::{tag, take_until}, character::complete::{char, digit1}, error::ErrorKind, sequence::{preceded, terminated, tuple}, IResult};
+use nom_locate::LocatedSpan;
 use ornaments::{AocError, Solution};
+
+type Span<'a> = LocatedSpan<&'a str>;
 
 pub use crate::Day3 as Day;
 
@@ -74,7 +77,7 @@ impl<P> Day3<P> {
     fn parse_mul(input: &str) -> IResult<&str, (&str, &str)> {
         let (input, trash) = take_until("mul(")(input)?;
 
-        dbg!(trash);
+        // dbg!(trash);
 
         preceded(
             tag("mul("),
@@ -94,12 +97,12 @@ impl<P> Day3<P> {
 
         while !input.is_empty() {
         // loop {
-            dbg!(input);
+            // dbg!(input);
 
             // let (remainder, product) = Self::parse_mul(input).unwrap();
             match Self::parse_mul(input) {
                 Ok((remainder, product)) => {
-                    dbg!(remainder, product);
+                    // dbg!(remainder, product);
                     products.push(Product::new(product.0, product.1));
                     input = remainder;
                 },
@@ -107,11 +110,11 @@ impl<P> Day3<P> {
                 Err(nom::Err::Error(err)) => {
                     match err.code {
                         ErrorKind::Char if input.len() > 1 => {
-                            dbg!(err);
+                            // dbg!(err);
                             input = &input[1..];
                         }
                         ErrorKind::TakeUntil => {
-                            dbg!(err);
+                            // dbg!(err);
                             input = "";
                             // break;
                         }
@@ -119,7 +122,11 @@ impl<P> Day3<P> {
                         e => panic!("Error: {:?}", e),
                     }
                 }
-                Err(_) => break,
+                Err(e) => {
+                    dbg!(e);
+                    break;
+                }
+                // Err(_) => break,
             }
         }
 
@@ -190,58 +197,173 @@ impl Solution for Day3<Part2> {
     type Item = Vec<Product>;
 
     fn parse(input: &str) -> Self {
-        dbg!(input);
+        let input = Span::new(input);
+        let mut products = Vec::new();
+        
+        println!("Initial input: {:?}", input.fragment());
+        
+        // Get everything before first don't()
+        let (mut current, initial) = match take_until::<_, _, nom::error::Error<Span>>("don't()")(input) {
+            Ok((remainder, initial)) => {
+                println!("Before first don't(): {:?}", initial.fragment());
+                println!("Remainder after don't: {:?}", remainder.fragment());
+                
+                let (_, mut initial_products) = Day3::<Part2>::parse_all_mul(initial.fragment()).unwrap();
+                println!("Initial products: {:?}", initial_products);
+                products.extend(initial_products);
+                (remainder, initial)
+            }
+            Err(_) => {
+                let (_, products) = Day3::<Part2>::parse_all_mul(input.fragment()).unwrap();
+                return Day3(products, PhantomData);
+            }
+        };
 
-        let (remainder, part1) = take_until::<_, _, nom::error::Error<&str>>("don't()")(input).unwrap();
-
-        // dbg!(remainder, part1);
-
-        let (_, mut products) = Day3::<Part2>::parse_all_mul(part1).unwrap();
-        let mut input = remainder;
-
-        loop {
-            dbg!(input);
-
-
-            let (a, next) = take_until::<_, _, nom::error::Error<&str>>("do()")(input).unwrap();
-            dbg!(a, next);
-            let (remainder, mut new_products) = Day3::<Part2>::parse_all_mul(a).unwrap();
-            products.append(&mut new_products);
-
-            input = remainder;
-
-
-
-
-        //     // let (remainder, product) = Self::parse_mul(input).unwrap();
-        //     match Self::parse_mul(input) {
-        //         Ok((remainder, product)) => {
-        //             dbg!(remainder, product);
-        //             products.push(Product::new(product.0, product.1));
-        //             input = remainder;
-        //         },
-        //         // Handle various parsing errors by skipping invalid input
-        //         Err(nom::Err::Error(err)) => {
-        //             match err.code {
-        //                 ErrorKind::Char if input.len() > 1 => {
-        //                     input = &input[1..];
-        //                 }
-        //                 ErrorKind::TakeUntil => {
-        //                     // input = "";
-        //                     break;
-        //                 }
-        //                 // _ => return Err(nom::Err::Error(err)),
-        //                 e => panic!("Error: {:?}", e),
-        //             }
-        //         }
-        //         Err(_) => break,
-        //     }
+        while !current.is_empty() {
+            println!("\nProcessing section starting at: {:?}", current.fragment());
+            
+            // Skip don't()
+            let (after_dont, _) = tag::<_, _, nom::error::Error<Span>>("don't()")(current).unwrap();
+            println!("After don't(): {:?}", after_dont.fragment());
+            
+            // Find next do()
+            match take_until::<_, _, nom::error::Error<Span>>("do()")(after_dont) {
+                Ok((after_do, disabled_section)) => {
+                    println!("Disabled section: {:?}", disabled_section.fragment());
+                    
+                    // Skip do()
+                    let (remainder, _) = tag::<_, _, nom::error::Error<Span>>("do()")(after_do).unwrap();
+                    println!("Enabled section: {:?}", remainder.fragment());
+                    
+                    // Process enabled section until next don't()
+                    match take_until::<_, _, nom::error::Error<Span>>("don't()")(remainder) {
+                        Ok((next_dont, enabled)) => {
+                            println!("Processing until next don't(): {:?}", enabled.fragment());
+                            let (_, mut new_products) = Day3::<Part2>::parse_all_mul(enabled.fragment()).unwrap();
+                            products.extend(new_products);
+                            current = next_dont;
+                        }
+                        Err(_) => {
+                            // Process until end
+                            println!("Processing until end: {:?}", remainder.fragment());
+                            let (_, mut new_products) = Day3::<Part2>::parse_all_mul(remainder.fragment()).unwrap();
+                            products.extend(new_products);
+                            break;
+                        }
+                    }
+                }
+                Err(_) => {
+                    println!("No more do() found");
+                    break;
+                }
+            }
         }
 
-        dbg!(&products);
-
+        println!("\nFinal products: {:?}", products);
         Day3(products, PhantomData)
     }
+
+    // fn parse(input: &str) -> Self {
+    //     let input = Span::new(input);
+    //     let mut products = Vec::new();
+        
+    //     // Get everything before first don't()
+    //     let (mut current, initial) = match take_until::<_, _, nom::error::Error<Span>>("don't()")(input) {
+    //         Ok((remainder, initial)) => {
+    //             let (_, mut initial_products) = Day3::<Part2>::parse_all_mul(initial.fragment()).unwrap();
+    //             products.extend(initial_products);
+    //             (remainder, initial)
+    //         }
+    //         Err(_) => {
+    //             // No don't() found, process everything
+    //             let (_, products) = Day3::<Part2>::parse_all_mul(input.fragment()).unwrap();
+    //             return Day3(products, PhantomData);
+    //         }
+    //     };
+
+    //     while !current.is_empty() {
+    //         // Skip don't()
+    //         let (after_dont, _) = tag::<_, _, nom::error::Error<Span>>("don't()")(current).unwrap();
+            
+    //         // Find next do()
+    //         match take_until::<_, _, nom::error::Error<Span>>("do()")(after_dont) {
+    //             Ok((after_do, _disabled_section)) => {
+    //                 // Skip do()
+    //                 let (remainder, _) = tag::<_, _, nom::error::Error<Span>>("do()")(after_do).unwrap();
+                    
+    //                 // Process enabled section
+    //                 let (new_current, mut new_products) = Day3::<Part2>::parse_all_mul(remainder.fragment()).unwrap();
+    //                 products.extend(new_products);
+    //                 current = Span::new(new_current);
+    //             }
+    //             Err(_) => break,
+    //         }
+    //     }
+
+    //     Day3(products, PhantomData)
+    // }
+
+    // fn parse(input: &str) -> Self {
+    //     // dbg!(input);
+
+    //     let (remainder, part1) = take_until::<_, _, nom::error::Error<&str>>("don't()")(input).unwrap();
+
+    //     // dbg!(remainder, part1);
+
+    //     let (_, mut products) = Day3::<Part2>::parse_all_mul(part1).unwrap();
+    //     let mut input = remainder;
+
+    //     loop {
+    //         // dbg!(input);
+
+    //         match take_until::<_, _, nom::error::Error<&str>>("do()")(input) {
+    //             Ok((a, next)) => {
+    //                 assert!(next.starts_with("don't()"));
+    //                 // dbg!(a, next);
+    //                 let (remainder, mut new_products) = Day3::<Part2>::parse_all_mul(a).unwrap();
+    //                 products.append(&mut new_products);
+
+    //                 // input = remainder;
+    //                 input = next;
+    //             },
+    //             // Err(_) => break,
+
+    //             Err(e) => {
+    //                 dbg!(e);
+    //                 break;
+    //             }
+    //         }
+
+    //     //     // let (remainder, product) = Self::parse_mul(input).unwrap();
+    //     //     match Self::parse_mul(input) {
+    //     //         Ok((remainder, product)) => {
+    //     //             dbg!(remainder, product);
+    //     //             products.push(Product::new(product.0, product.1));
+    //     //             input = remainder;
+    //     //         },
+    //     //         // Handle various parsing errors by skipping invalid input
+    //     //         Err(nom::Err::Error(err)) => {
+    //     //             match err.code {
+    //     //                 ErrorKind::Char if input.len() > 1 => {
+    //     //                     input = &input[1..];
+    //     //                 }
+    //     //                 ErrorKind::TakeUntil => {
+    //     //                     // input = "";
+    //     //                     break;
+    //     //                 }
+    //     //                 // _ => return Err(nom::Err::Error(err)),
+    //     //                 e => panic!("Error: {:?}", e),
+    //     //             }
+    //     //         }
+    //     //         Err(_) => break,
+    //     //     }
+    //     }
+
+
+    //     // dbg!(&products);
+
+    //     Day3(products, PhantomData)
+    // }
 
     /// sums all products
     fn part1(&mut self) -> miette::Result<Self::Output, AocError> {
@@ -257,7 +379,6 @@ impl Solution for Day3<Part2> {
         Ok(output)
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -278,18 +399,4 @@ mod tests {
         assert_eq!("48", Day3::<Part2>::parse(input).solve(Part::Two)?);
         Ok(())
     }
-
-    // #[test]
-    // fn test_part3() -> miette::Result<()> {
-    //     let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-    //     assert_eq!("96", Day3::parse(input).solve(Part::Two)?);
-    //     Ok(())
-    // }
-
-    // #[test]
-    // fn test_part4() -> miette::Result<()> {
-    //     let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-    //     assert_eq!("144", Day3::parse(input).solve(Part::Two)?);
-    //     Ok(())
-    // }
 }
