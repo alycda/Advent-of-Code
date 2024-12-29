85aedd08efa958b26181e29a2083e07e4aa13d72
Alyssa Evans <alyssa.evans@pluto.tv>
2024-12-29 08:35:14 +0000

    2024 day 3 refactor part 2 WIP

--- 2024/rust/day-03/src/bin/part1.rs
+++ 2024/rust/day-03/src/bin/part1.rs
@@ -1,7 +1,7 @@
 use ornaments::Solution;
 use miette::Context;
 
-use day_03::Day;
+use day_03::{Day, Part1};
 
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
@@ -16,7 +16,7 @@ fn main() -> miette::Result<()> {
     tracing_subscriber::fmt::init();
 
     let file = include_str!("../../input.txt");
-    let result = Day::parse(file).solve(ornaments::Part::One).context("process part 1")?;
+    let result = Day::<Part1>::parse(file).solve(ornaments::Part::One).context("process part 1")?;
 
     println!("{}", result);
     Ok(())
--- 2024/rust/day-03/src/lib.rs
+++ 2024/rust/day-03/src/lib.rs
@@ -13,17 +13,45 @@
 //! * The don't() instruction disables future mul instructions.
 //!     - Only the most recent do() or don't() instruction applies.
 
-use nom::{
-    bytes::complete::{tag, take_until}, character::complete::{char, digit1}, error::ErrorKind, sequence::{preceded, terminated, tuple}, IResult
-};
+use std::{marker::PhantomData, num::ParseIntError, str::FromStr};
 
-use ornaments::{Solution, AocError};
+use nom::{bytes::complete::{tag, take_until}, character::complete::{char, digit1}, error::ErrorKind, sequence::{preceded, terminated, tuple}, IResult};
+use ornaments::{AocError, Solution};
 
 pub use crate::Day3 as Day;
+
+#[derive(Debug)]
+pub struct Part1;
+
+#[derive(Debug)]
+pub struct Part2;
+
+#[derive(Debug)]
+pub struct Day3<P>(Vec<Product>, PhantomData<P>);
+
+impl<P> std::ops::Deref for Day3<P> {
+    type Target = Vec<Product>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 /// Represents a multiplication operation with two operands
 #[derive(Debug, Clone, Copy)]
 pub struct Product(usize, usize);
+
+impl FromStr for Product {
+    // type Err = IResult;
+    // type Err = miette::Error;
+    type Err = ParseIntError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let (a, b) = input.split_once(",").expect("valid input");
+        
+        Ok(Product(a.parse()?, b.parse()?))
+    }
+}
 
 impl Product {
     /// Creates a new Product from string representations of numbers
@@ -31,7 +59,7 @@ impl Product {
     /// # Panics
     /// Panics if either string cannot be parsed as usize
     pub fn new(a: &str, b: &str) -> Self {
-        Self(a.parse().expect("is a number"), b.parse().expect("is a number"))
+        Self(a.parse().expect("a number"), b.parse().expect("a number"))
     }
 
     /// Computes the product of the two numbers
@@ -40,92 +68,106 @@ impl Product {
     }
 }
 
-/// Parses multiplication expressions in the EXACT format "mul(x,y)"
-fn parse_mul(input: &str) -> IResult<&str, (&str, &str)> {
-    let (input, _) = take_until("mul(")(input)?;
-    preceded(
-        tag("mul("),
-        terminated(
-            tuple((
-                digit1,
-                // consume the comma
-                preceded(char(','), digit1),
-            )),
-            char(')'),
-        ),
-    )(input)
-}
+impl<P> Day3<P> {
 
-/// Represents possible instructions in the program
-#[derive(Debug)]
-enum Instruction {
-    Mul(Product),
-    Do,
-    Dont,
-}
+    /// Parses multiplication expressions in the EXACT format "mul(x,y)"
+    fn parse_mul(input: &str) -> IResult<&str, (&str, &str)> {
+        let (input, trash) = take_until("mul(")(input)?;
 
-#[derive(Debug, Clone)]
-pub struct Day3(Vec<Product>);
+        dbg!(trash);
 
-impl Day3 {
-    fn parse_all_products(mut input: &str) -> nom::IResult<&str, Vec<Product>, nom::error::Error<&str>> {
+        preceded(
+            tag("mul("),
+            terminated(
+                tuple((
+                    digit1,
+                    // consume the comma
+                    preceded(char(','), digit1),
+                )),
+                char(')'),
+            ),
+        )(input)
+    }
+
+    fn parse_all_mul(mut input: &str) -> IResult<&str, Vec<Product>> {
         let mut products = Vec::new();
 
         while !input.is_empty() {
-            match parse_mul(input) {
-                Ok((remainder, (a, b))) => {
-                    products.push(Product::new(a, b));
+        // loop {
+            dbg!(input);
+
+            // let (remainder, product) = Self::parse_mul(input).unwrap();
+            match Self::parse_mul(input) {
+                Ok((remainder, product)) => {
+                    dbg!(remainder, product);
+                    products.push(Product::new(product.0, product.1));
                     input = remainder;
-                }
+                },
                 // Handle various parsing errors by skipping invalid input
                 Err(nom::Err::Error(err)) => {
                     match err.code {
                         ErrorKind::Char if input.len() > 1 => {
+                            dbg!(err);
                             input = &input[1..];
                         }
                         ErrorKind::TakeUntil => {
+                            dbg!(err);
                             input = "";
+                            // break;
                         }
-                        _ => return Err(nom::Err::Error(err)),
+                        // _ => return Err(nom::Err::Error(err)),
+                        e => panic!("Error: {:?}", e),
                     }
                 }
-                Err(e) => return Err(e),
+                Err(_) => break,
             }
         }
 
         Ok((input, products))
     }
 
-    fn _handle_parse_error<'a>(input: &'a str, err: nom::error::Error<&str>) -> miette::Result<&'a str, AocError> {
-        match err.code {
-            ErrorKind::Char if input.len() > 1 => Ok(&input[1..]),
-            ErrorKind::TakeUntil => Ok(""),
-            // _ => Err(AocError::ParseError(format!("Parse error: {err:?}"))),
-            _ => todo!(),
-        }
-    }
 }
 
-impl std::ops::Deref for Day3 {
-    type Target = Vec<Product>;
-    
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Solution for Day3 {
+impl Solution for Day3<Part1> {
     type Output = usize;
     type Item = Vec<Product>;
 
     fn parse(input: &str) -> Self {
-        Day3::nom_parser(input)
-            .map(|(_, products)| Self(products))
-            .unwrap_or_else(|e| panic!("Failed to parse input: {e}"))
-    }
+        // let mut products = Vec::new();
+        // let mut input = input;
 
-    fn nom_parser(input: &str) -> nom::IResult<&str, Self::Item, nom::error::Error<&str>> {
-        Day3::parse_all_products(input)
+        // while !input.is_empty() {
+        // // loop {
+        //     dbg!(input);
+
+        //     // let (remainder, product) = Self::parse_mul(input).unwrap();
+        //     match Self::parse_mul(input) {
+        //         Ok((remainder, product)) => {
+        //             dbg!(remainder, product);
+        //             products.push(Product::new(product.0, product.1));
+        //             input = remainder;
+        //         },
+        //         // Handle various parsing errors by skipping invalid input
+        //         Err(nom::Err::Error(err)) => {
+        //             match err.code {
+        //                 ErrorKind::Char if input.len() > 1 => {
+        //                     input = &input[1..];
+        //                 }
+        //                 ErrorKind::TakeUntil => {
+        //                     input = "";
+        //                     // break;
+        //                 }
+        //                 // _ => return Err(nom::Err::Error(err)),
+        //                 e => panic!("Error: {:?}", e),
+        //             }
+        //         }
+        //         Err(_) => break,
+        //     }
+        // }
+
+        let (_, products) = Day3::<Part1>::parse_all_mul(input).unwrap();
+
+        Day3(products, PhantomData)
     }
 
     /// sums all products
@@ -136,7 +178,86 @@ impl Solution for Day3 {
 
         Ok(output)
     }
+
+    /// sums all products
+    fn part2(&mut self) -> miette::Result<Self::Output, AocError> {
+        unimplemented!("Part 2")
+    }
 }
+
+impl Solution for Day3<Part2> {
+    type Output = usize;
+    type Item = Vec<Product>;
+
+    fn parse(input: &str) -> Self {
+        dbg!(input);
+
+        let (remainder, part1) = take_until::<_, _, nom::error::Error<&str>>("don't()")(input).unwrap();
+
+        // dbg!(remainder, part1);
+
+        let (_, mut products) = Day3::<Part2>::parse_all_mul(part1).unwrap();
+        let mut input = remainder;
+
+        loop {
+            dbg!(input);
+
+
+            let (a, next) = take_until::<_, _, nom::error::Error<&str>>("do()")(input).unwrap();
+            dbg!(a, next);
+            let (remainder, mut new_products) = Day3::<Part2>::parse_all_mul(a).unwrap();
+            products.append(&mut new_products);
+
+            input = remainder;
+
+
+
+
+        //     // let (remainder, product) = Self::parse_mul(input).unwrap();
+        //     match Self::parse_mul(input) {
+        //         Ok((remainder, product)) => {
+        //             dbg!(remainder, product);
+        //             products.push(Product::new(product.0, product.1));
+        //             input = remainder;
+        //         },
+        //         // Handle various parsing errors by skipping invalid input
+        //         Err(nom::Err::Error(err)) => {
+        //             match err.code {
+        //                 ErrorKind::Char if input.len() > 1 => {
+        //                     input = &input[1..];
+        //                 }
+        //                 ErrorKind::TakeUntil => {
+        //                     // input = "";
+        //                     break;
+        //                 }
+        //                 // _ => return Err(nom::Err::Error(err)),
+        //                 e => panic!("Error: {:?}", e),
+        //             }
+        //         }
+        //         Err(_) => break,
+        //     }
+        }
+
+        dbg!(&products);
+
+        Day3(products, PhantomData)
+    }
+
+    /// sums all products
+    fn part1(&mut self) -> miette::Result<Self::Output, AocError> {
+        unimplemented!("Part 1")
+    }
+
+    /// sums all products
+    fn part2(&mut self) -> miette::Result<Self::Output, AocError> {
+        let output: Self::Output = self.iter()
+            .map(|p| p.value())
+            .sum();
+
+        Ok(output)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -147,14 +268,28 @@ mod tests {
     #[test]
     fn test_part1() -> miette::Result<()> {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        assert_eq!("161", Day3::parse(input).solve(Part::One)?);
+        assert_eq!("161", Day3::<Part1>::parse(input).solve(Part::One)?);
         Ok(())
     }
 
     #[test]
     fn test_part2() -> miette::Result<()> {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        assert_eq!("48", Day3::parse(input).solve(Part::Two)?);
+        assert_eq!("48", Day3::<Part2>::parse(input).solve(Part::Two)?);
         Ok(())
     }
+
+    // #[test]
+    // fn test_part3() -> miette::Result<()> {
+    //     let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    //     assert_eq!("96", Day3::parse(input).solve(Part::Two)?);
+    //     Ok(())
+    // }
+
+    // #[test]
+    // fn test_part4() -> miette::Result<()> {
+    //     let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+    //     assert_eq!("144", Day3::parse(input).solve(Part::Two)?);
+    //     Ok(())
+    // }
 }
